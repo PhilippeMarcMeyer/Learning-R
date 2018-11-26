33cb13c5ff3cbddbf0879d35920156636c07fecd
philippe MEYER <pmg.meyer@gmail.com>
2018-11-26 21:24:03 +0100

    improvement : percentage

--- loremipsum1.R
+++ loremipsum1.R
@@ -1,14 +1,35 @@
-text<-scan("files/loremipsum.txt", what="character", sep="\n")
-#text<-scan(choose.files(), what="character", sep="\n")
+text<-scan("files/loremipsum.txt", 
+           what="character", 
+           sep="\n")
 
-words_list <- strsplit(text, "\\W+", perl=TRUE)
-tolower(words_list[])
+text_v <- tolower(text)
 
-words_vector <- unlist(words_list)
-nrWords <- length(words_vector)
-df <- data.frame(words = words_vector)
-freq <- table(df$words)
-freq_sorted <-sort(freq,decreasing=TRUE)
-df <-  as.data.frame(freq_sorted)
-df$pc  <- nrWords
-df$pc <- df$Freq / df$pc
+text_v <- gsub("<", "", text_v)
+text_v <- gsub(">", "", text_v)
+text_v <- gsub("  ", " ", text_v)
+
+text_v <- paste(text_v, sep = " ", collapse = "")
+
+text_words_l <- strsplit(text_v, "\\W")
+text_words_v <- unlist(text_words_l)
+text_words_v <- text_words_v[text_words_v != ""]
+
+text_freq_t <- table(text_words_v)
+text_freq_sorted_t <- sort(words=text_freq_t, decreasing = TRUE)
+
+nrWords <- length(text_freq_sorted_t)
+text_freq_rel_t <- (text_freq_sorted_t / nrWords) * 100
+
+text_freq_all_sorted_t <-
+  cbind(nr=text_freq_sorted_t, pc=text_freq_rel_t)
+
+#percentage_v <- text_freq_all_sorted_t[,1]
+#select_vector <- percentage_v >= 5
+#text_freq_all_top5pc_t <- text_freq_all_sorted_t[select_vector,]
+
+
+
+
+
+
+
